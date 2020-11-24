abe8f6ff5fa568f6ee89f809b231b9b2e068235a
melnik3997 <melnik3997@gmail.com>
2020-11-24 22:35:40 +0300

    авторизация

--- src/main/java/com/example/rma/GreetingController.java
+++ src/main/java/com/example/rma/GreetingController.java
@@ -1,27 +1,56 @@
 package com.example.rma;
 
+import com.example.rma.domain.Message;
+import com.example.rma.repository.MessageRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RequestPart;
 
+import java.util.List;
 import java.util.Map;
 
 @Controller
 public class GreetingController {
+    @Autowired
+    private MessageRepository messageRepository;
 
-    @GetMapping("/greeting")
-    public String greeting(
-            @RequestParam(name="name", required=false, defaultValue="World"
-            ) String name, Map<String, Object> model)
-    {
-        model.put("name", name);
+    @GetMapping("/")
+    public String greeting( String name, Map<String, Object> model) {
         return "greeting";
     }
-    @GetMapping
+    @GetMapping("/main")
     public String main(Map<String, Object> model ){
-        model.put("some", "Hello, test!!!");
+        Iterable<Message> messages = messageRepository.findAll();
+        model.put("messages", messages);
         return "main";
     }
+
+    @PostMapping("/main")
+    public String addMessage(@RequestPart String text,
+                             @RequestPart String tag,
+                             Map<String, Object> model)
+    {
+        Message message =  new Message(text, tag);
+        messageRepository.save(message);
+        return "main";
+    }
+
+    @PostMapping("filter")
+    public String filter(@RequestPart(required=false) String filter,
+                         Map<String, Object> model) {
+        Iterable<Message> messageList;
+        if(filter!= null && !filter.isEmpty()) {
+            messageList = messageRepository.findByTag(filter);
+        }else
+        {
+            messageList = messageRepository.findAll();
+        }
+        model.put("messages",messageList);
+        return "main";
+    }
+
 
 }
---
+++ src/main/java/com/example/rma/repository/MessageRepository.java
@@ -0,0 +1,14 @@
+package com.example.rma.repository;
+
+import com.example.rma.domain.Message;
+import org.springframework.data.repository.CrudRepository;
+
+import java.util.List;
+
+
+public interface MessageRepository extends CrudRepository<Message, Long>
+{
+    List<Message> findByTag(String tag);
+
+}
+
